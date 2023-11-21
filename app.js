7f6f59746831056b5f31e515b4d8ab4fb2156fc6
DhruvS28 <45272029+DhruvS28@users.noreply.github.com>
2023-11-21 18:24:10 -0500

    Working rotten tomatoes api call

--- app.js
+++ app.js
@@ -1,24 +1,72 @@
-const http = require('http');
-const fs = require("fs");
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
 const port = 3000;
 
-const server = http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	fs.readFile('index.html', function(error, data) {
-		if (error) {
-			res.writeHead(400)
-			res.write("Error: File not found.")
-		} else {
-			res.write(data)
-		}
-		res.end();
-	})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, function(error) {
-	if (error) {
-		console.log('Something went wrong!\n', error);
-	} else {
-		console.log('Server is listening on port ', port);
-	}
+app.post('/getMovieInfo', async (req, res) => {
+  const movieName = req.body.movieName;
+
+  try {
+	//https://rotten-tomatoes-api.ue.r.appspot.com/docs#/
+    const response = await axios.get(`https://rotten-tomatoes-api.ue.r.appspot.com/search/${movieName}`);
+    const movieInfo = response.data;
+    let tableRows = '';
+
+    if (Array.isArray(movieInfo.movies)) {
+      // Handle multiple movies
+      movieInfo.movies.forEach(movie => {
+        tableRows += `
+          <tr>
+            <td>
+              <p>Title: ${movie.name}</p>
+            </td>
+            <td>
+              <p>Audience: ${movie.audience_score}</p>
+              <p>Tomato: ${movie.tomatometer}</p>
+              <p>Genres: ${movie.genres.join(', ')}</p>
+              <p>Duration: ${movie.duration}</p>
+              <p>Release Year: ${movie.year}</p>
+            </td>
+          </tr>
+        `;
+      });
+    } else {
+      // Handle a single movie
+      tableRows = `
+        <tr>
+          <td>
+            <p>Title: ${movieInfo.name}</p>
+          </td>
+          <td>
+            <p>Audience: ${movieInfo.audience_score}</p>
+            <p>Tomato: ${movieInfo.tomatometer}</p>
+            <p>Genres: ${movieInfo.genres.join(', ')}</p>
+            <p>Duration: ${movieInfo.duration}</p>
+            <p>Release Year: ${movieInfo.year}</p>
+          </td>
+        </tr>
+      `;
+    }
+
+    res.send(`
+      <h2>Movies Information</h2>
+      <table border="1">
+        ${tableRows}
+      </table>
+    `);
+  } catch (error) {
+    res.send('Error fetching movie information. Please try again.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
